353cd970989c8d2b1526fe7bcf144ec42e3b6b21
John Saunders <john@venturenet.co.za>
2015-01-25 15:24:14 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+##
+## Function : makeCacheMatrix (m)
+##
+## Purpose : Function containing logic to save and retrieve the inverse of a matrix from cache.
+##
+## Syntax : getsol(x) : return a copy of the inverse of x from cache or NULL if none exists.
+##        : setsol(x) : save a copy of the inverse of x in cache.
+##
+## Returns: getsol() : return a copy from cache if it exists or NULL if not.
+##          setsol() : save a copy of the solution in cache and return the result.
+##
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsol <- function(solve) m <<- solve
+  getsol <- function() m
+  list(set = set, get = get, 
+       setsol = setsol, 
+       getsol=getsol)
 }
 
 
-## Write a short comment describing this function
-
+## 
+##  Function : cacheSolve (m)
+##
+##  Purpose : Function to return the inverse of matrix m from and store it in cache if 
+##            it does not already exist in cache.
+##
+##  Syntax  : cacheSolve(m) where m is the matrix of which the inverse is required.
+##
+##  Returns : The inverse of the matrix m retrieved from cache. If a copy does not exists in
+##            cache, a copy is stored in cache and the result returned.
+##
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getsol()
+  if (!is.null(m)){
+    return (m)
+    m <- solve(m)
+    x$setsol(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsol(m)
 }
